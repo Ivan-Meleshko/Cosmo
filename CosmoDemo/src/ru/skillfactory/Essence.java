c1e1c484389e69713238bd5cc39bfe465fd0074c
Ivan Meleshko <ivanmeleshko@gmail.com>
2022-05-09 20:49:49 +0300

    console version: loop added, case of the literals input described

--- CosmoDemo/src/ru/skillfactory/Essence.java
+++ CosmoDemo/src/ru/skillfactory/Essence.java
@@ -5,20 +5,48 @@ import java.util.Scanner;
 public class Essence {
 
 	MyWindow mywindow;
+	Boolean isOn = true;
+	char x;
+	
 
 	public Essence() {
-
-		System.out.println("Find out the Day tribe and the Ascension tribe of the number. Enter the number...");
-
+		
+		while (isOn) {
+		System.out.println("Find out the Day tribe and the Ascension tribe of the number. Enter the number.");
+		
 		Scanner scan = new Scanner(System.in);
+		boolean hasInt = scan.hasNextInt();
+		
+		if (hasInt) {
 		int k = scan.nextInt();
-
 		if (k <= 0) {
-			System.out.println("Please, enter the correct number!");
+			System.out.println("The number must be positive!");		
 		} else {
 			System.out.printf("Number %d: Day tribe number is %s. Ascension tribe is %s.", k,
 					MyWindow.calculateTribe(k), MyWindow.calculateAscensionTribe(k));
+			System.out.println();
+		System.out.println("Press 'x' to exit, any key for another run.");
+		x = scan.next().charAt(0); 
+		if (x == 'x') {
+			System.out.println("Goodbye.");
+			isOn = false;
+			System.exit(0);
+			scan.close();
 		}
+		}
+		} else if (!hasInt) {
+			String b = scan.next(); // заглушка
+			System.out.println("Error! Not a number...");
+		}
+			
+		}
+			
 	}
-
+			
+		
+		
+				
+		
+		
+	
 }
