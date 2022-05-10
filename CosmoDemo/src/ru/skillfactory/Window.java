8f07660a01cbe65d5b71c868bfb511b97520b69a
Ivan Meleshko <ivanmeleshko@gmail.com>
2022-05-10 12:40:18 +0300

    window version: empty window

---
+++ CosmoDemo/src/ru/skillfactory/Window.java
@@ -0,0 +1,22 @@
+package ru.skillfactory;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
+public class Window extends JFrame {
+	
+	
+	public Window() {
+	setBounds(300, 300, 330, 500);
+	setResizable(false);
+	setDefaultCloseOperation(3);
+	
+	
+	// "Find out the Day tribe and the Ascension tribe of the number. Enter the number.";
+	
+	setVisible(true);
+
+	}
+}
